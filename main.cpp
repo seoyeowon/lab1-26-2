ca743868a4f61695c27e65c526b214a466768b93
seoyeowon <seoyeowon@ewha.ac.kr>
2026-09-07 04:23:55 +0000

    2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
 int main(){
+    int numofstudents(30); numofstudents = 37;
+    std::cin >> numofstudents;
+    std::cout << numofstudents << std::endl;
+    // std::cout << sizeof(numofstudents) << std::endl;
+    // std::cout << typeid(numofstudents).name() << std::endl;
+    // std::cout << typeid(static_cast<double>(numofstudents)).name() << std::endl;
     return 0;
 }
